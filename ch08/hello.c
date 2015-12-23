12c39f4c3cf5504b556235f8d8745543da470dc0
Pratool Gadtaula <pratool.gadtaula@students.olin.edu>
2015-12-23 02:30:31 -0500

    Understood more of Glib 2.0 implementation of doubly linked list and came up with a makefile using path variables for including and linking glibc libraries

--- ch08/hello.c
+++ ch08/hello.c
@@ -4,6 +4,16 @@
 int main(int argc, char** argv) {
     GList* list = NULL;
     list = g_list_append(list, "Hello world!");
+    int i;
+    for (i = 0; i < 5; i++) {
+        list = g_list_append(list, "new item");
+    }
     printf("The first item is '%s'\n", g_list_first(list)->data);
+    i = 0;
+    GList* l;
+    for (l = list; l != NULL; l = l->next) {
+        printf("item #%d: '%s'\n", i, l->data);
+        i++;
+    }
     return 0;
 }
